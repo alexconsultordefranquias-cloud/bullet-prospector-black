afb5c4a75a1d87a46114d4295ab2d8ae7a670d94
alexconsultordefranquias-cloud <alex.consultordefranquias@gmail.com>
2026-09-16 16:50:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
-# --- A GRANDE MURALHA (BLACKLIST MULTI-SETORIAL) ---
+# --- LISTA NEGRA 1: MARCAS GIGANTES (AGORA COM EDUCAÇÃO) ---
 BLACKLIST = [
-    # Alimentação e Fast Food
+    # Alimentação
     "mcdonald", "burger king", "subway", "bob's", "bobs", "madero", "kfc", "giraffas", "outback", "habib",
-    "cacau show", "china in box", "spoleto", "dominos", "domino's", "pizza hut", "coco bambu", "jerônimo", "jeronimo",
-    
-    # Educação
+    # Academias/Estética
+    "smart fit", "smartfit", "skyfit", "bluefit", "panobianco", "pratique", "selfit", "gavioes", "gaviões",
+    "espaço laser", "espaçolaser", "laser fast", "emagrecentro", "giorgio",
+    # Educação, Cursos e Instituições (NOVA BLINDAGEM)
     "microlins", "kumon", "wizard", "fisk", "ccaa", "yázigi", "yazigi", "cna", "minds", "influx", 
     "senai", "senac", "sesi", "fatec", "etec", "grau técnico", "grau tecnico", "cebrac", "embelleze",
-    "prepara", "faculdade", "universidade", "unip", "estácio", "estacio", "anhanguera", "mackenzie", "myrobot",
-    
-    # Estética, Beleza e Academias
-    "smart fit", "smartfit", "skyfit", "bluefit", "panobianco", "pratique", "selfit", "gavioes", "gaviões",
-    "espaço laser", "espaçolaser", "laser fast", "emagrecentro", "giorgio", "sobrancelhas design", "giolaser", "onodera",
-    
-    # Perfumaria
-    "boticário", "boticario", "natura", "avon", "quem disse", "água de cheiro", "agua de cheiro", "l'occitane", "loccitane",
-    
-    # Sorveterias
-    "chiquinho", "bacio di latte", "oggi", "sr sorvete", "los paleteros", "ice cream roll",
-    
-    # Lavanderias
-    "5asec", "5àsec", "lavateria", "dryclean usa", "prima clean", "quality lavanderia",
-    
-    # Tintas
-    "coral", "suvinil", "decor colors", "tintas mc", "lukscolor",
-    
-    # Pet Shop
-    "petz", "cobasi", "petland", "zee.dog", "zeedog",
-    
-    # Energia
-    "hcc energia", "portal solar", "blue sol", "solar prime",
-    
-    # Automotivos, Barbearia e Serviços Gerais
-    "bosch", "localiza", "unidas", "movida", "mercadocar", "dpk", "porto seguro", "getninjas", "corleone", "seu elias"
+    "prepara", "faculdade", "universidade", "unip", "estácio", "estacio", "anhanguera", "mackenzie"
 ]
